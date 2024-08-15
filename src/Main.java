0cde0017d92c899b79e5d348209ea971fa72a45e
Derek <dnq@uw.edu>
2024-08-15 11:42:12 -0700

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import java.util.*;
+//import java.util.*;
 import javax.swing.*;
 
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        }
+            JFrame frame = new JFrame("Flashcard Manager");
+            frame.setVisible(true);
+            frame.setBounds(400, 400, 1200, 800);
     }
 }
